609c67dfd3d28c3665445850310854fa70e043c6
Chris Pollard <cspollard@gmail.com>
2011-02-26 18:20:25 -0500

    takes into account average scores home and away.

--- get_rankings.py
+++ get_rankings.py
@@ -25,35 +25,39 @@ c = crawler(filename=argv[1])
 teams = c.teams().values()
 games = c.games().values()
 
-for t in teams:
-    if t.ngames() < 5:
-        for g in t.games():
-            t.opponent(g).remove_game(g)
-            t.remove_game(g)
-            games.remove(g)
-
-        print "removing", t.name()
-        stdout.flush()
-        teams.remove(t)
-
-
 l = len(teams)
 team_dict = dict(zip(teams, xrange(l)))
 scores = zeros((l, l))
 
 n = datetime.now()
 
+home_tot = 0
+away_tot = 0
+haratios = 0
+nhomegames = 0
 for g in games:
-    t1, t2 = g.teams()
-    if t1 not in team_dict or t2 not in team_dict:
-        print "attempting to use nonexistent team."
-        print t1.name(), t2.name()
-        stdout.flush()
-        continue
+    if g.home() != -1:
+        home_tot += g.score(g.home_team())
+        away_tot += g.score(g.away_team())
+        haratios += g.score_ratio(g.home_team())
+        nhomegames += 1
 
-    p1, p2 = g.score(t1), g.score(t2)
+home_bonus = (home_tot+away_tot)/(2.*home_tot)
+away_bonus = (home_tot+away_tot)/(2.*away_tot)
+print "home bonus: %4f, away bonus: %4f" % (home_bonus, away_bonus)
+
+for g in games:
+    if g.home() == -1:
+        t1, t2 = g.teams()
+        p1, p2 = g.score(t1), g.score(t2)
+    else:
+        t1, t2 = g.home_team(), g.away_team()
+        p1 = g.score(t1)*home_bonus
+        p2 = g.score(t2)*away_bonus
+
     scores[team_dict[t1]][team_dict[t2]] += p1
     scores[team_dict[t2]][team_dict[t1]] += p2
+
 
 a = alg(scores)
 vec = a.minimize()
