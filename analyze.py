b3f2fefb2012e988f8676ef09e3862aa7905d7e5
Chris <cspollard@gmail.com>
2011-11-19 16:13:58 -0500

    now can get probability matrix.

--- analyze.py
+++ analyze.py
@@ -1,42 +1,40 @@
 # analyzes data coming from the crawler.
+from sys import argv
 from crawler import crawl
-from scipy.linalg import det, block_diag, expm
+from scipy.linalg import expm3, expm2
+from teamslist import teamslist
 
-(teamsdict, (homescores, awayscores, neutscores)) = crawl()
+def main():
+    (teamsdict, (homescores, awayscores, neutscores)) = crawl(filename=argv[1])
 
-# normalize scores.
-# homenorm = homescores.sum()/(homescores != 0).sum()
-# awaynorm = awayscores.sum()/(awayscores != 0).sum()
-# neutnorm = neutscores.sum()/(neutscores != 0).sum()
+    # normalize scores.
+    homenorm = homescores.sum()/(homescores != 0).sum()
+    awaynorm = awayscores.sum()/(awayscores != 0).sum()
+    neutnorm = neutscores.sum()/(neutscores != 0).sum()
 
-# homescores /= homenorm
-# awayscores /= awaynorm
-# neutscores /= neutnorm
+    homescores /= homenorm
+    awayscores /= awaynorm
+    neutscores /= neutnorm
 
-scores = homescores + awayscores + neutscores
+    scores = homescores + awayscores + neutscores
 
-sym = (scores + scores.T) / 2
-asym = (scores - scores.T) / 2
-normed = scores/(2*sym + (sym == 0))
-print block_diag(scores)
-print det(scores)
-print det(sym)
-print det(asym)
-print det(normed)
-exped = expm(normed, 15)
-print det(exped)
+    sym = (scores + scores.T) / 2
+    asym = (scores - scores.T) / 2
+    normed = scores/(2*sym + (sym == 0))
+    exped = expm3(normed, 50)
 
-for team1 in teamsdict:
-        print team1, exped[teamsdict[team1], teamsdict[team1]]
+    teamval = []
+    for team in teamsdict:
+        teamval.append([team,
+            exped[teamsdict[team]][0]/exped[0][teamsdict[team]],
+            exped[teamsdict[team]][1]/exped[1][teamsdict[team]],
+            exped[teamsdict[team]][2]/exped[2][teamsdict[team]]])
 
-"""
-        if neutscores[teamsdict[team1], teamsdict[team2]]:
-            print team1, neutscores[teamsdict[team1], \
-                    teamsdict[team2]], team2, \
-                    neutscores[teamsdict[team2], teamsdict[team1]], "N"
+    teamval = sorted(teamval, key=lambda t: -t[1])
 
-        if homescores[teamsdict[team1], teamsdict[team2]]:
-            print team1, homescores[teamsdict[team1], \
-                    teamsdict[team2]], team2, \
-                    awayscores[teamsdict[team2], teamsdict[team1]]
-"""
+    for team in teamval:
+        print team[0].rjust(30), "%2f %2f %2f %2f" % (team[1], team[2],
+                team[3], team[1]/team[2])
+
+if __name__ == "__main__":
+    main()
