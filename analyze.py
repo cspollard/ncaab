a61cedcf9301da737bdeb71019a6e8efdcc16f4e
Chris <cspollard@gmail.com>
2011-11-19 23:11:48 -0500

    added energy minimum method.

--- analyze.py
+++ analyze.py
@@ -1,14 +1,62 @@
 # analyzes data coming from the crawler.
 from sys import argv
 from crawler import crawl
-from numpy import diag
-from scipy.linalg import expm3, expm2
+from numpy import diag, abs as nabs, dot
+from scipy.linalg import expm3, expm2, svd
 from teamslist import teamslist
 
 def main():
     (teamsdict, (homescores, awayscores, neutscores)) = crawl(filename=argv[1])
 
     # normalize scores.
+    # homescores, awayscores, neutscores = norm_venues(homescores,
+            # awayscores, neutscores)
+
+    # get final scores matrix.
+    scores = homescores + awayscores + neutscores
+
+    vals = prob_network(scores)
+
+    print vals
+
+    teamval = []
+    for team, n in teamsdict.items():
+        teamval.append([team, vals[n]])
+
+    teamval = sorted(teamval, key=lambda t: -t[1])
+
+    for team in teamval:
+        print team[0].rjust(30), "%2f" % (team[1])
+
+
+def prob_network(scores):
+    # get known score fractions.
+    normed = scores/((scores + scores.T) + (scores == 0))
+    exped = expm3(normed, 50)
+
+    print (exped < 0).sum()
+    # get rid of diagonal part.
+    exped -= diag(diag(exped))
+
+    # divide by symmetric part.
+    probs = exped/((exped + exped.T) + (exped == 0))
+    print ((exped + exped.T) + (exped == 0) < .001).sum()
+    probs = probs.sum(axis=1)
+    probs /= (exped != 0).sum(axis=1)
+
+    return probs
+
+
+def energy_min(scores):
+    # get known score fractions.
+    normed = scores/((scores + scores.T) + (scores == 0))
+
+    M = diag((normed*normed).sum(axis=0)) - (normed*normed.T)
+
+    return nabs(svd(M)[-1][-1])
+
+
+def norm_venues(homescores, awayscores, neutscores):
     homenorm = homescores.sum()/(homescores != 0).sum()
     awaynorm = awayscores.sum()/(awayscores != 0).sum()
     neutnorm = neutscores.sum()/(neutscores != 0).sum()
@@ -17,27 +65,8 @@ def main():
     awayscores /= awaynorm
     neutscores /= neutnorm
 
-    scores = homescores + awayscores + neutscores
+    return homescores, awayscores, neutscores
 
-    sym = (scores + scores.T) / 2
-    # asym = (scores - scores.T) / 2
-    normed = scores/(2*sym + (sym == 0))
-    # ratio = scores/(scores.T + (scores.T == 0))
-    exped = expm2(normed)
-    exped -= diag(diag(exped))
-    probs = exped/((exped + exped.T) + (exped == 0))
-    probs = probs.sum(axis=1)
-    probs /= (exped != 0).sum(axis=1) - 1
-
-    teamval = []
-    for team, n in teamsdict.items():
-        teamval.append([team, probs[n]])
-
-    teamval = sorted(teamval, key=lambda t: -t[1])
-
-    norm = teamval[0][1]
-    for team in teamval:
-        print team[0].rjust(30), "%2f" % (team[1]/norm)
 
 if __name__ == "__main__":
     main()
