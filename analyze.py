1ae9bd8a92bc7b10b6a6afc14a44c59f2d6041b1
Chris <cspollard@gmail.com>
2011-11-21 08:02:29 -0500

    no changes really.

--- analyze.py
+++ analyze.py
@@ -4,21 +4,40 @@ from crawler import crawl
 from numpy import diag, abs as nabs, dot, nan_to_num
 from scipy.linalg import expm3, expm2, svd
 from teamslist import teamslist
+from itertools import permutations
 
 def main():
     (teamsdict, (homescores, awayscores, neutscores)) = \
-            crawl(filename=argv[1], update=0)
+            crawl(update=0, bb=0)
 
     # normalize scores.
-    # homescores, awayscores, neutscores = norm_venues(homescores,
-            # awayscores, neutscores)
+    homescores, awayscores, neutscores = norm_venues(homescores,
+            awayscores, neutscores)
 
     # get final scores matrix.
     scores = homescores + awayscores + neutscores
-    scores = (scores > scores.T)
+    # scores = (scores > scores.T)
 
-    vals = prob_network(scores)
+    # games = [("North Carolina", "Mississippi Valley St."), ("Alabama",
+        # "Purdue"), ("North Carolina", "Duke"), ("Duke",
+            # "North Carolina"), ("Old Dominion", "Kentucky"),
+            # ("Mississippi", "Marquette")]
+
+    # games = permutations(["Alabama", "Arkansas", "Oklahoma", \
+            # "Virginia Tech", "Oklahoma State", "Louisiana State",
+            # "Stanford", "Oregon", "Southern Cal"], 2)
+    # print_probs(teamsdict, scores, games)
+
+    # print; print
+
+    vals = prob_network_ratings(scores)
     # vals = energy_min(scores)
+
+    print_values(teamsdict, vals)
+
+
+
+def print_values(teamsdict, vals):
 
     teamval = []
     for team, n in teamsdict.items():
@@ -30,6 +49,12 @@ def main():
         print (teamval[i][0] + " %03d" % (i+1)).rjust(30), "%2f" % (teamval[i][1])
 
 
+def print_probs(teamsdict, scores, games):
+    probs = prob_network(scores)
+    for g in games:
+        print g[0].rjust(30) + ' : ' + g[1].ljust(30), probs[teamsdict[g[0]],teamsdict[g[1]]]
+
+
 def prob_network(scores):
     # get known score fractions.
     normed = scores/((scores + scores.T) + (scores == 0))
@@ -39,10 +64,14 @@ def prob_network(scores):
     exped -= diag(diag(exped))
 
     # divide by symmetric part.
-    probs = exped/((exped + exped.T) + (exped == 0))
-    probs = probs.sum(axis=1)
-    sums = (exped != 0).sum(axis=1)
+    return exped/((exped + exped.T) + (exped == 0))
+
+
+def prob_network_ratings(scores):
+    probs = prob_network(scores) 
+    sums = (probs != 0).sum(axis=1)
     sums += (sums == 0)
+    probs = probs.sum(axis=1)
     probs /= sums
 
     return probs
